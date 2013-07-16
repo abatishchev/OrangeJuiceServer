4ebf5356ac58bd07b3f7f17670c5fd9fdc43b554
Alex Batishchev <v-albati@microsoft.com>
2013-07-16 16:52:05 -0700

    Enabling JSON formatter (by removing XML formatter)

--- OrangeJuice.Server.Api/App_Start/WebApiConfig.cs
+++ OrangeJuice.Server.Api/App_Start/WebApiConfig.cs
@@ -12,6 +12,9 @@ namespace OrangeJuice.Server.Api
 				defaults: new { id = RouteParameter.Optional }
 			);
 
+            var formatters = config.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
 			// Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
 			// To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
 			// For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
