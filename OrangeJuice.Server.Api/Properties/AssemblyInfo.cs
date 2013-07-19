eb1a702aa00e4b630a47beabb0fe5c374bc16d1b
Alex Batishchev <v-albati@microsoft.com>
2013-07-19 14:55:13 -0700

    Strong name signing stub

--- OrangeJuice.Server.Api/Properties/AssemblyInfo.cs
+++ OrangeJuice.Server.Api/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.1.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("OrangeJuice.Server.Api.Test")]
+[assembly: InternalsVisibleTo("OrangeJuice.Server.Api.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd3e4c0c8d601a29f1f7a568c115b23593736d9fc020c5c9acdc9e53b55cb3bcc4248462b79890cb29815c410a09825918ea79a2cbbff0f9d06da3885885c299a130349884f0dcb9d29f0debf876095f223301a284cbe8981baeb276d162a58b27ffbc02bbc1748cc05b6612cbb6798e01d22bafae2a8093742fc01002d44bc5")]
