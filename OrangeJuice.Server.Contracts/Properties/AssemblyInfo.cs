eeb05acd13ceb3bc17b7c05b03c29095c78b7678
Alexander Batishchev <abatishchev@gmail.com>
2013-07-20 19:08:01 -0700

    Adding SharedAssemblyInfo

--- OrangeJuice.Server.Contracts/Properties/AssemblyInfo.cs
+++ OrangeJuice.Server.Contracts/Properties/AssemblyInfo.cs
@@ -2,37 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("OrangeJuice.Server.Contracts")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Orange Juice")]
 [assembly: AssemblyProduct("OrangeJuice.Server.Contracts")]
-[assembly: AssemblyCopyright("Copyright © Orange Juice 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cda83f85-40c8-49db-ba14-dcc19722d9cd")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("OrangeJuice.Server.Contracts.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd3e4c0c8d601a29f1f7a568c115b23593736d9fc020c5c9acdc9e53b55cb3bcc4248462b79890cb29815c410a09825918ea79a2cbbff0f9d06da3885885c299a130349884f0dcb9d29f0debf876095f223301a284cbe8981baeb276d162a58b27ffbc02bbc1748cc05b6612cbb6798e01d22bafae2a8093742fc01002d44bc5")]
-[assembly: InternalsVisibleTo("OrangeJuice.Server.Api.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd3e4c0c8d601a29f1f7a568c115b23593736d9fc020c5c9acdc9e53b55cb3bcc4248462b79890cb29815c410a09825918ea79a2cbbff0f9d06da3885885c299a130349884f0dcb9d29f0debf876095f223301a284cbe8981baeb276d162a58b27ffbc02bbc1748cc05b6612cbb6798e01d22bafae2a8093742fc01002d44bc5")]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Orange Juice")]
+[assembly: AssemblyCopyright("Copyright © Orange Juice 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// 0.0.0.0 means a local build. Real version will be calculated and assigned by the server build
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
