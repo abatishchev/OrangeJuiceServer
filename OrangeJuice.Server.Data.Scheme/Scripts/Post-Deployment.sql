8e156daee967029ba5378069ba492f203755d051
Alexander Batishchev <abatishchev@gmail.com>
2013-07-20 19:56:52 -0700

    Adding sql server structure. Adding deployment, security scripts

---
+++ OrangeJuice.Server.Data.Scheme/Scripts/Post-Deployment.sql
@@ -0,0 +1,27 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from dbo.[AccountType])
+begin
+	insert into dbo.[AccountType] ([AccountTypeId], [Name])
+	values
+	(1, 'Checking'),
+	(2, 'Saving')
+end
+go
+
+if not exists (select 1 from dbo.[Category])
+begin
+	insert into dbo.[Category] ([Name])
+	values ('(None)')
+end
+go
