3f55648f52e94251d2016c0fe63160c8054dfc39
Alexander Batishchev <abatishchev@gmail.com>
2013-07-20 19:56:09 -0700

    Adding entity model project

---
+++ OrangeJuice.Server.Data.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeJuice.Server.Data.Model")]
+[assembly: AssemblyCompany("Godfather Software Foundation")]
+[assembly: AssemblyProduct("OrangeJuice.Server.Data.Model")]
+
+[assembly: Guid("2cc86c7b-46b7-421c-843c-0f4f2a491d2e")]
+[assembly: InternalsVisibleTo("OrangeJuice.Server.Data.Model.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd3e4c0c8d601a29f1f7a568c115b23593736d9fc020c5c9acdc9e53b55cb3bcc4248462b79890cb29815c410a09825918ea79a2cbbff0f9d06da3885885c299a130349884f0dcb9d29f0debf876095f223301a284cbe8981baeb276d162a58b27ffbc02bbc1748cc05b6612cbb6798e01d22bafae2a8093742fc01002d44bc5")]
