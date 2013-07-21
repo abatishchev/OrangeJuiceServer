cffa02d6d0a9104ad55046348f087ed8282fadd5
Alexander Batishchev <abatishchev@gmail.com>
2013-07-20 22:15:46 -0700

    Removing key from InternalsVisibleTo attributes

--- OrangeJuice.Server.Api/Properties/AssemblyInfo.cs
+++ OrangeJuice.Server.Api/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("98a7790e-80c4-4a93-9ff3-f7970ff03a9c")]
 
-[assembly: InternalsVisibleTo("OrangeJuice.Server.Api.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd3e4c0c8d601a29f1f7a568c115b23593736d9fc020c5c9acdc9e53b55cb3bcc4248462b79890cb29815c410a09825918ea79a2cbbff0f9d06da3885885c299a130349884f0dcb9d29f0debf876095f223301a284cbe8981baeb276d162a58b27ffbc02bbc1748cc05b6612cbb6798e01d22bafae2a8093742fc01002d44bc5")]
+[assembly: InternalsVisibleTo("OrangeJuice.Server.Api.Test")]
---
+++ OrangeJuice.Server.Data.Model.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeJuice.Server.Data.Model.Test")]
+[assembly: AssemblyProduct("OrangeJuice.Server.Data.Model.Test")]
+
+[assembly: Guid("32cfabf8-b642-42b9-b0e4-de18bb6f391d")]
