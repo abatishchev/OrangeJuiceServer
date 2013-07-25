0cb46b82dae0a77f197d1f2cb427de49c5d069cd
Alex Batishchev <v-albati@microsoft.com>
2013-07-25 15:53:34 -0700

    Updating AppKeyFilter, deleteing UserKeyFilter, QueryFilterBase

--- OrangeJuice.Server.Api/Filters/AppKeyFilter.cs
+++ OrangeJuice.Server.Api/Filters/AppKeyFilter.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace OrangeJuice.Server.Api.Filters
 {
-	internal sealed class AppKeyFilter : QueryFilterBase
+	internal sealed class AppKeyFilter : System.Web.Http.Filters.ActionFilterAttribute
 	{
-		protected override string QuerySegmentName
+		private const string AppKeySegmentName = "appKey";
+
+		public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext context)
 		{
-			get { return "appKey"; }
+			string appKey = GetAppKey(context.Request.RequestUri.Query);
+
+			Guid guid;
+			if (!Guid.TryParse(appKey, out guid) || guid != AppKey.Version0)
+			{
+				var response = context.Request.CreateErrorResponse(System.Net.HttpStatusCode.Unauthorized, "You can't use the API without application key");
+				throw new System.Web.Http.HttpResponseException(response);
+			}
+		}
+
+		private static string GetAppKey(string query)
+		{
+			return System.Web.HttpUtility.ParseQueryString(query).Get(AppKeySegmentName);
 		}
 	}
 }
