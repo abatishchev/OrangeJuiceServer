c3a40d66bdec1307509b23c41d1c2a9ac3626504
Alex Batishchev <v-albati@microsoft.com>
2013-07-24 17:33:18 -0700

    Removing anciently added back guid as string validation

--- OrangeJuice.Server.Api/Models/ApiKey.cs
+++ OrangeJuice.Server.Api/Models/ApiKey.cs
@@ -6,7 +6,6 @@ namespace OrangeJuice.Server.Api.Models
     public class ApiKey
     {
         [Required]
-        [StringLength(36)]
         public Guid AppKey { get; set; }
 
         public Guid? UserKey { get; set; }
