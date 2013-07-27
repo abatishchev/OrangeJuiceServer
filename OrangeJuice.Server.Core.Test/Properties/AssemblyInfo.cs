632fe8d27972ca2f8fbc77291535b75fe1372fbd
Alexander Batishchev <abatishchev@gmail.com>
2013-07-26 22:30:16 -0700

    Adding the Core project with tests

---
+++ OrangeJuice.Server.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeJuice.Server.Core.Test")]
+[assembly: AssemblyProduct("OrangeJuice.Server.Core.Test")]
+
+[assembly: Guid("05958ec3-23a2-45cc-87c8-1d3242d58fdf")]
---
+++ OrangeJuice.Server.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeJuice.Server.Core")]
+[assembly: AssemblyProduct("OrangeJuice.Server.Core")]
+
+[assembly: Guid("07a3badc-0ac2-4f36-b848-47900e7f8e84")]
+
+#if !DEBUG
+// attributes to be updated by the server build
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+#endif
+
+[assembly: InternalsVisibleTo("OrangeJuice.Server.Core.Test")]
