34f6fb9f184d3628f991b44f4a132a3280ca3aa1
Alexander Batishchev <abatishchev@gmail.com>
2013-07-27 00:23:22 -0700

    Better ObjectContextExtensions

--- OrangeJuice.Server.Api.Test/ObjectContextExtensions.cs
+++ OrangeJuice.Server.Api.Test/ObjectContextExtensions.cs
@@ -1,18 +1,25 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 
 namespace OrangeJuice.Server.Api.Test
 {
-    public static class ObjectContextExtensions
-    {
-        public static T GetValue<T>(this ObjectContent context)
-        {
-            ObjectContent<T> genericContext = context as ObjectContent<T>;
-            return genericContext != null ? genericContext.GetValue() : (T)context.Value;
-        }
+	public static class ObjectContextExtensions
+	{
+		public static T GetValue<T>(this HttpContent context)
+		{
+			ObjectContent<T> objectContent = context as ObjectContent<T>;
+			return objectContent != null ? objectContent.GetValue() : default(T);
+		}
 
-        public static T GetValue<T>(this ObjectContent<T> context)
-        {
-            return (T)context.Value;
-        }
-    }
+		public static T GetValue<T>(this ObjectContent context)
+		{
+			ObjectContent<T> genericContext = context as ObjectContent<T>;
+			return genericContext != null ? genericContext.GetValue() : default(T);
+		}
+
+		public static T GetValue<T>(this ObjectContent<T> context)
+		{
+			return (T)context.Value;
+		}
+	}
 }
