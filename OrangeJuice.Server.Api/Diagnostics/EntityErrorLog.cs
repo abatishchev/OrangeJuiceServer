15d350717486bf89b65fc643e061d88f03e9ff7e
Alexander Batishchev <abatishchev@gmail.com>
2013-07-28 01:53:08 -0700

    Adding EntityErrorLog

---
+++ OrangeJuice.Server.Api/Diagnostics/EntityErrorLog.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity.Core.EntityClient;
+
+namespace OrangeJuice.Server.Api.Diagnostics
+{
+	public class EntityErrorLog : Elmah.SqlErrorLog
+	{
+		private readonly string _connectionStringName;
+
+		public EntityErrorLog(System.Collections.IDictionary config)
+			: base(config)
+		{
+			_connectionStringName = (string)config["connectionStringName"];
+		}
+
+		public override string ConnectionString
+		{
+			get
+			{
+				string entityConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings[_connectionStringName].ConnectionString;
+				EntityConnectionStringBuilder builder = new EntityConnectionStringBuilder(entityConnectionString);
+				return builder.ProviderConnectionString;
+			}
+		}
+	}
+}
