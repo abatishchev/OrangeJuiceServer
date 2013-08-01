7c75fc3985e88eb98563dd6fa0f88e4ee7924b6f
Alexander Batishchev <abatishchev@gmail.com>
2013-07-31 20:50:08 -0700

    Adding TempConnectionString

---
+++ OrangeJuice.Server.Api.Test/Configuration/TempConnectionString.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace OrangeJuice.Server.Api.Test.Configuration
+{
+	internal class TempConnectionString : IDisposable
+	{
+		private readonly string _name;
+		private readonly string _oldValue;
+
+		public TempConnectionString(string name, string value)
+		{
+			_name = name;
+			ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings[name];
+			if (connectionString != null)
+			{
+				_oldValue = connectionString.ConnectionString;
+				connectionString.ConnectionString = value;
+			}
+			else
+			{
+				UpdateValue(name, value);
+			}
+		}
+
+		private static void UpdateValue(string name, string value)
+		{
+			var connectionString = new ConnectionStringSettings(name, value, "System.Data.SqlClient");
+
+			var readonlyField = typeof(ConfigurationElementCollection).GetField("bReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+			readonlyField.SetValue(ConfigurationManager.ConnectionStrings, false);
+
+			var baseAddMethod = typeof(ConfigurationElementCollection).GetMethod("BaseAdd", BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(ConfigurationElement) }, null);
+			baseAddMethod.Invoke(ConfigurationManager.ConnectionStrings, new[] { connectionString });
+
+			readonlyField.SetValue(ConfigurationManager.ConnectionStrings, true);
+		}
+
+		public void Dispose()
+		{
+			if (_oldValue != null)
+				UpdateValue(_name, _oldValue);
+			//else
+				//UpdateValue(_name, "");
+		}
+	}
+}
--- OrangeJuice.Server.Api.Test/Diagnostics/EntityErrorLogTest.cs
+++ OrangeJuice.Server.Api.Test/Diagnostics/EntityErrorLogTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Configuration;
-
-using Elmah;
 
 using FluentAssertions;
 
@@ -20,16 +17,21 @@ namespace OrangeJuice.Server.Api.Test.Diagnostics
 		public void ConnectionString_Should_Return_Provider_Part_From_Connection_String()
 		{
 			// Arrange
+			const string name = "Test";
 			const string expected = "Data Source=localhost;Initial Catalog=Test;Integrated Security=True;";
 			string entityConnectionString = String.Format("metadata=res://*/;provider=System.Data.SqlClient;provider connection string=&quot;{0}&quot;", expected);
-			IDictionary config = new Dictionary<string, string> { { EntityErrorLog.ConnectionStringNameKey, "Test" } };
-			EntityErrorLog errorLog = new EntityErrorLog(config);
 
-			// Act
-			string actual = errorLog.ConnectionString;
+			using (new Configuration.TempConnectionString(name, entityConnectionString))
+			{
+				IDictionary config = new Dictionary<string, string> { { EntityErrorLog.ConnectionStringNameKey, name } };
+				EntityErrorLog errorLog = new EntityErrorLog(config);
 
-			// Assert
-			actual.Should().Be(expected);
+				// Act
+				string actual = errorLog.ConnectionString;
+
+				// Assert
+				actual.Should().Be(expected);
+			}
 		}
 	}
 }
