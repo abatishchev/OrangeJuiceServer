dc0daa3e491adca39ae331328d97dd500ed022e5
Alexander Batishchev <abatishchev@gmail.com>
2013-08-10 17:24:47 -0700

    Adding SQL part of Elmah

---
+++ OrangeJuice.Server.Data.Scheme/Schemas/dbo/Programmability/Stored Procedures/ELMAH_GetErrorXml.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE dbo.[ELMAH_GetErrorXml]
+(
+    @Application NVARCHAR(60),
+    @ErrorId UNIQUEIDENTIFIER
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT 
+        [AllXml]
+    FROM 
+        [ELMAH_Error]
+    WHERE
+        [ErrorId] = @ErrorId
+    AND
+        [Application] = @Application
+END
+GO
---
+++ OrangeJuice.Server.Data.Scheme/Schemas/dbo/Tables/Indexes/IX_ELMAH_Error_App_Time_Seq.sql
@@ -0,0 +1,8 @@
+﻿CREATE NONCLUSTERED INDEX [IX_ELMAH_Error_App_Time_Seq] ON dbo.[ELMAH_Error] 
+(
+    [Application]   ASC,
+    [TimeUtc]       DESC,
+    [Sequence]      DESC
+) 
+ON [PRIMARY]
+GO
