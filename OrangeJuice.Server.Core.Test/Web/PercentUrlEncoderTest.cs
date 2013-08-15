e87fa1ec8e9bc29c592ab72f8183a765dc7f6a42
Alexander Batishchev <abatishchev@gmail.com>
2013-08-15 12:23:31 -0700

    Adding tests to PercentUrlEncoderTest

--- OrangeJuice.Server.Core.Test/Web/PercentUrlEncoderTest.cs
+++ OrangeJuice.Server.Core.Test/Web/PercentUrlEncoderTest.cs
@@ -1,11 +1,66 @@
 ﻿using System;
+
+using FluentAssertions;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OrangeJuice.Server.Web;
 
 namespace OrangeJuice.Server.Test.Web
 {
 	[TestClass]
 	public class PercentUrlEncoderTest
 	{
-		// TODO: tests
+		[TestMethod]
+		public void Encode_Should_Aggregate_EncodingSteps_By_Url()
+		{
+			// Arrange
+			bool called = false;
+			var encodingSteps = new Func<string, string>[]
+			{
+				s =>
+					{
+						called = true;
+						return s;
+					}
+			};
+			IUrlEncoder urlEncoder = new PercentUrlEncoder(encodingSteps);
+
+			// Act
+			urlEncoder.Encode("anyUrl");
+
+			// Assert
+			called.Should().BeTrue();
+		}
+
+		[TestMethod]
+		public void PercentEncode_Should_Encode_Special_Characters_Using_Dictionary()
+		{
+			// Arrange
+			var charDir = PercentUrlEncoder.CreateCharacterDictionary();
+
+			foreach (var pair in charDir)
+			{
+				// Act
+				string encoded = PercentUrlEncoder.PercentEncode(pair.Key);
+
+				// Assert
+				encoded.Should().Be(pair.Value);
+			}
+		}
+
+		[TestMethod]
+		public void UpperCaseEncoding_Should_UpperCase_Encoded_Characters_And_Should_Not_Regular_Characters()
+		{
+			// Arrange
+			const string input = "word%ab%cde%25";
+			const string expected = "word%AB%CDe%25";
+
+			// Act
+			string actual = PercentUrlEncoder.UpperCaseEncoding(input);
+
+			// Assert
+			actual.Should().Be(expected);
+		}
 	}
 }
--- OrangeJuice.Server.Core/Web/PercentUrlEncoder.cs
+++ OrangeJuice.Server.Core/Web/PercentUrlEncoder.cs
@@ -1,41 +1,77 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
 
 namespace OrangeJuice.Server.Web
 {
 	public sealed class PercentUrlEncoder : IUrlEncoder
 	{
+		#region FIelds
+		private static readonly Lazy<IDictionary<string, string>> _charDir = new Lazy<IDictionary<string, string>>(CreateCharacterDictionary);
+
+		private readonly IEnumerable<Func<string, string>> _encodingSteps;
+		#endregion
+
+		#region Constructors
+		public PercentUrlEncoder()
+			: this(GetDefaultEncodingSteps())
+		{
+
+		}
+
+		internal PercentUrlEncoder(IEnumerable<Func<string, string>> encodingSteps)
+		{
+			_encodingSteps = encodingSteps;
+		}
+		#endregion
+
+		#region IUrlEncoder members
 		/// <summary>
-		/// Percent-encodes (URL Encode) according to RFC 3986 as required by Amazon.
+		/// Percent-encodes according to RFC 3986 as required by Amazon
 		/// </summary>
 		/// <remarks>
 		/// This is necessary because .NET's HttpUtility.UrlEncode does not encode according to the above standard.
-		/// Also, .NET returns lower-case encoding by default and Amazon requires upper-case encoding.
+		/// Also it returns lower-case encoding by default and Amazon requires upper-case encoding.
 		/// </remarks>
 		public string Encode(string url)
 		{
-			url = System.Web.HttpUtility.UrlEncode(url, Encoding.UTF8);
-			url = url.Replace("'", "%27")
-					 .Replace("(", "%28")
-					 .Replace(")", "%29")
-					 .Replace("*", "%2A")
-					 .Replace("!", "%21")
-					 .Replace("%7e", "~")
-					 .Replace("+", "%20");
-
-			StringBuilder sb = new StringBuilder(url);
-			for (int i = 0; i < sb.Length; i++)
-			{
-				if (sb[i] != '%')
-					continue;
-
-				if (!Char.IsLetter(sb[i + 1]) && !Char.IsLetter(sb[i + 2]))
-					continue;
-
-				sb[i + 1] = Char.ToUpperInvariant(sb[i + 1]);
-				sb[i + 2] = Char.ToUpperInvariant(sb[i + 2]);
-			}
-			return sb.ToString();
+			return _encodingSteps.Aggregate(url, (s, func) => func(s));
 		}
+		#endregion
+
+		#region Methods
+		internal static IDictionary<string, string> CreateCharacterDictionary()
+		{
+			return new Dictionary<string, string>
+			{
+				{ "'", "%27" },
+				{ "(", "%28" },
+				{ ")", "%29" },
+				{ "*", "%2A" },
+				{ "!", "%21" },
+				{ "~", "%7E" },
+				{ "+", "%2B" },
+			};
+		}
+
+		private static IEnumerable<Func<string, string>> GetDefaultEncodingSteps()
+		{
+			yield return s => HttpUtility.UrlEncode(s);
+			yield return PercentEncode;
+			yield return UpperCaseEncoding;
+		}
+
+		internal static string PercentEncode(string url)
+		{
+			return _charDir.Value.Aggregate(url, (str, pair) => str.Replace(pair.Key, pair.Value));
+		}
+
+		internal static string UpperCaseEncoding(string url)
+		{
+			return Regex.Replace(url, "%([a-z]{2})", m => '%' + m.Groups[1].Value.ToUpperInvariant());
+		}
+		#endregion
 	}
 }
