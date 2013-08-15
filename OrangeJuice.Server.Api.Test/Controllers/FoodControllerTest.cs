8400386a446f5d184578b32451b7ec142698603e
Alexander Batishchev <abatishchev@gmail.com>
2013-08-15 12:30:00 -0700

    Adding tests to FoodControllerTest

--- OrangeJuice.Server.Api.Test/Controllers/FoodControllerTest.cs
+++ OrangeJuice.Server.Api.Test/Controllers/FoodControllerTest.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 
+using FluentAssertions;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OrangeJuice.Server.Api.Controllers;
+using OrangeJuice.Server.Api.Models;
+using OrangeJuice.Server.Api.Services;
 
 namespace OrangeJuice.Server.Api.Test.Controllers
 {
 	[TestClass]
 	public class FoodControllerTest
 	{
+		#region Ctor
 		[TestMethod]
-		public void GetDescription_Should_()
+		public void Ctor_Should_Throw_Exception_When_AwsOptions_Is_Null()
+		{
+			// Arrange
+			const AwsOptions awsOptions = null;
+			const GroceryDescriptionFactory groceryDescriptionFactory = null;
+
+			// Act
+			Action action = () => new FoodController(awsOptions, groceryDescriptionFactory);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>()
+				  .And.ParamName.Should().Be("awsOptions");
+		}
+
+		[TestMethod]
+		public void Ctor_Should_Throw_Exception_When_GroceryDescriptionFactorys_Is_Null()
+		{
+			// Arrange
+			AwsOptions awsOptions = new AwsOptions();
+			const GroceryDescriptionFactory groceryDescriptionFactory = null;
+
+			// Act
+			Action action = () => new FoodController(awsOptions, groceryDescriptionFactory);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>()
+				  .And.ParamName.Should().Be("groceryDescriptionFactory");
+		}
+		#endregion
+
+
+		#region GetDescription
+		[TestMethod]
+		public void GetDescription_Should_Bla()
 		{
 			// TODO: tests
 		}
+		#endregion
 	}
 }
