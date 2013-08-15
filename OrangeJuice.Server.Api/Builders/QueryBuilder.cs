519c0a2ecdedf29cc30d8dbefb1cd2a822da2d2d
Alexander Batishchev <abatishchev@gmail.com>
2013-08-14 17:26:31 -0700

    Adding tests to QueryBuilderTest

--- OrangeJuice.Server.Api/Builders/QueryBuilder.cs
+++ OrangeJuice.Server.Api/Builders/QueryBuilder.cs
@@ -8,12 +8,22 @@ namespace OrangeJuice.Server.Api.Builders
 {
 	internal class QueryBuilder
 	{
+		internal const string AwsAccessKeyId = "AWSAccessKeyId";
+		internal const string Timestamp = "Timestamp";
+
 		private readonly string _accessKey;
 		private readonly IUrlEncoder _urlEncoder;
 		private readonly IDateTimeProvider _dateTimeProvider;
 
 		public QueryBuilder(string accessKey, IUrlEncoder urlEncoder, IDateTimeProvider dateTimeProvider)
 		{
+			if (String.IsNullOrEmpty(accessKey))
+				throw new ArgumentNullException("accessKey");
+			if (urlEncoder == null)
+				throw new ArgumentNullException("urlEncoder");
+			if (dateTimeProvider == null)
+				throw new ArgumentNullException("dateTimeProvider");
+
 			_accessKey = accessKey;
 			_urlEncoder = urlEncoder;
 			_dateTimeProvider = dateTimeProvider;
@@ -25,8 +35,9 @@ namespace OrangeJuice.Server.Api.Builders
 			dic = new SortedDictionary<string, string>(dic, StringComparer.Ordinal);
 
 			// Add the AWSAccessKeyId and Timestamp to the requests.
-			dic["AWSAccessKeyId"] = _accessKey;
-			dic["Timestamp"] = _dateTimeProvider.FormatToUniversal(DateTime.UtcNow);
+			dic[AwsAccessKeyId] = _accessKey;
+			DateTime now = _dateTimeProvider.GetNow();
+			dic[Timestamp] = _dateTimeProvider.FormatToUniversal(now);
 
 			// Get the canonical query string
 			return String.Join("&",
