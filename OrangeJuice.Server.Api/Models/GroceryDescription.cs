8a46e54e3cfbaa77a14b97cce73355e0108fa3c6
Alexander Batishchev <abatishchev@gmail.com>
2013-08-16 23:03:45 -0700

    Implementing GroceryDescriptionFactory

--- OrangeJuice.Server.Api/Models/GroceryDescription.cs
+++ OrangeJuice.Server.Api/Models/GroceryDescription.cs
@@ -2,6 +2,15 @@
 {
 	public class GroceryDescription
 	{
-		public string Name { get; set; }
+		// ReSharper disable once InconsistentNaming
+		public string ASIN { get; set; }
+
+		public string Title { get; set; }
+
+		public string Manufacturer { get; set; }
+
+		public string DetailPageUrl { get; set; }
+
+		public string TechnicalDetailsUrl { get; set; }
 	}
 }
--- OrangeJuice.Server.Api/Models/GroceryDescriptionFactory.cs
+++ OrangeJuice.Server.Api/Models/GroceryDescriptionFactory.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace OrangeJuice.Server.Api.Models
 {
-	public class GroceryDescriptionFactory
+	public sealed class GroceryDescriptionFactory
 	{
 		public GroceryDescription Create(XElement element)
 		{
-			throw new NotImplementedException();
+			XNamespace ns = element.Name.Namespace;
+			XElement itemAttributes = element.Element(ns + "ItemAttributes");
+
+			return new GroceryDescription
+			{
+				ASIN = (string)element.Element(ns + "ASIN"),
+				Title = (string)itemAttributes.Element(ns + "Title"),
+				Manufacturer = (string)itemAttributes.Element(ns + "Manufacturer"),
+
+				DetailPageUrl = (string)element.Element(ns + "DetailPageUrl"),
+				TechnicalDetailsUrl = (string)element.Elements(ns + "ItemLink")
+													 .Single(e => (string)e.Element(ns + "Description") == "Technical Details")
+													 .Element(ns + "URL"),
+			};
 		}
 	}
 }
