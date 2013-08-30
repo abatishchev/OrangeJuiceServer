6eb5ea1f7532f242b1d7640fb433ecccea69ab90
Alexander Batishchev <abatishchev@gmail.com>
2013-08-29 23:08:23 -0700

    Adding integration tests project

---
+++ OrangeJuice.Server.Api.Test.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeJuice.Server.Api.Test.Integration")]
+[assembly: AssemblyProduct("OrangeJuice.Server.Api.Test.Integration")]
+
+[assembly: Guid("30bc5175-78f8-4d4d-bd0d-371dfdb33f4f")]
