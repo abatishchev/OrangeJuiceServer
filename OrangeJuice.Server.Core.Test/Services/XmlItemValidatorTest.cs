638b3c50aaa17c35df9e53062f4bce9951de7baa
Alex Batishchev <abatishchev@gmail.com>
2013-09-23 18:10:59 -0700

    Adding missed XmlItemValidatorTest

---
+++ OrangeJuice.Server.Core.Test/Services/XmlItemValidatorTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml.Linq;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OrangeJuice.Server.Services;
+
+namespace OrangeJuice.Server.Test.Services
+{
+	[TestClass]
+	public class XmlItemValidatorTest
+	{
+		[TestMethod]
+		public void IsValid_Should_Throw_Exception()
+		{
+			// Arange
+			const XElement item = null;
+
+			IValidator<XElement> validator = new XmlItemValidator();
+
+			// Act
+			Action action = () => validator.IsValid(item);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>()
+				  .And.ParamName.Should().Be("item");
+		}
+
+		[TestMethod]
+		public void IsValid_Should_Return_Request_IsValid_Element_Value()
+		{
+			// Arange
+			const bool expected = true;
+			XNamespace ns = "test";
+			XElement item = new XElement(ns + "Item",
+				new XElement(ns + "Request",
+					new XElement(ns + "IsValid",
+						new XText(expected.ToString()))));
+
+			IValidator<XElement> validator = new XmlItemValidator();
+
+			// Act
+			bool actual = validator.IsValid(item);
+
+			// Assert
+			actual.Should().Be(expected);
+		}
+	}
+}
