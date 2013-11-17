4dcad91288936dc8deb2b3d38fbea19083f47136
Alex Batishchev <abatishchev@gmail.com>
2013-11-16 21:07:32 -0800

    Updating

--- OrangeJuice.Server.Core/Filters/ValidImageFoodDescriptionFilter.cs
+++ OrangeJuice.Server.Core/Filters/ValidImageFoodDescriptionFilter.cs
@@ -1,18 +1,27 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using OrangeJuice.Server.Data;
 
 namespace OrangeJuice.Server.Filters
 {
-	public class ValidImageFoodDescriptionFilter : IFilter<FoodDescription>
+	public sealed class ValidImageFoodDescriptionFilter : IFilter<FoodDescription>
 	{
-		public bool Filter(FoodDescription fd)
+		public bool Filter(FoodDescription foodDescription)
 		{
-			if (fd == null)
+			if (foodDescription == null)
 				throw new ArgumentNullException("foodDescription");
 
-			return new[] { fd.LargeImageUrl, fd.MediumImageUrl, fd.SmallImageUrl }.All(u => u != null);
+			var urls = GetUrls(foodDescription);
+			return urls.All(u => u != null);
+		}
+
+		private static IEnumerable<object> GetUrls(FoodDescription fd)
+		{
+			yield return fd.LargeImageUrl;
+			yield return fd.MediumImageUrl;
+			yield return fd.SmallImageUrl;
 		}
 	}
 }
