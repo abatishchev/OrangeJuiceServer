9f7c047240d9a53cd17644787ddb76aa69d12aae
Alex Batishchev <abatishchev@gmail.com>
2013-12-17 16:31:22 -0800

    Getting rid of async MoqExtensions in favor of the new built-in

--- OrangeJuice.Server.Api.Test/Controllers/UserControllerTest.cs
+++ OrangeJuice.Server.Api.Test/Controllers/UserControllerTest.cs
@@ -12,7 +12,6 @@ using Moq;
 using OrangeJuice.Server.Api.Controllers;
 using OrangeJuice.Server.Api.Models;
 using OrangeJuice.Server.Data;
-using OrangeJuice.Server.Test;
 
 namespace OrangeJuice.Server.Api.Test.Controllers
 {
