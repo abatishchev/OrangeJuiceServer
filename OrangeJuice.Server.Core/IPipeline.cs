5c60aee58bd675606a7f5a0fddfe062d755c863c
Alex Batishchev <abatishchev@gmail.com>
2013-12-27 20:58:14 -0800

    Adding IPipeline<T>

---
+++ OrangeJuice.Server.Core/IPipeline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrangeJuice.Server
+{
+	public interface IPipeline<T>
+	{
+		IEnumerable<Func<T, T>> GetOperations();
+	}
+
+	public static class PipelineExtensions
+	{
+		public static T Run<T>(this IPipeline<T> pipeline, T input)
+		{
+			return pipeline.GetOperations().Aggregate(input, (param, op) => op(param));
+		}
+	}
+}
