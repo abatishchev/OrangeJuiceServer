b42b1edfd9999fa85162905a4ef7b4e94ad2b4c4
Alex Batishchev <abatishchev@gmail.com>
2014-01-23 11:23:27 -0800

    Removing irrelevant test

--- OrangeJuice.Server.Core.Test/Data/AwsFoodRepositoryTest.cs
+++ OrangeJuice.Server.Core.Test/Data/AwsFoodRepositoryTest.cs
@@ -6,7 +6,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
 using OrangeJuice.Server.Data;
-using OrangeJuice.Server.Filters;
 using OrangeJuice.Server.Services;
 
 namespace OrangeJuice.Server.Test.Data
@@ -53,38 +52,10 @@ namespace OrangeJuice.Server.Test.Data
 			// Assert
 			factoryMock.Verify(f => f.Create(element), Times.Once);
 		}
-
-		[TestMethod]
-		public async Task Search_Should_Pass_Each_Item_Returned_By_AwsProvider_SearhItems_To_FoodDescriptionFilter_Filter()
-		{
-			// Arrange
-			const string title = "anyTitle";
-			XElement element = new XElement("item");
-			FoodDescription description = new FoodDescription();
-
-			var providerMock = new Mock<IAwsProvider>();
-			providerMock.Setup(c => c.SearchItems(title)).ReturnsAsync(new[] { element });
-
-			var factoryMock = new Mock<IFoodDescriptionFactory>();
-			factoryMock.Setup(f => f.Create(element)).Returns(description);
-
-			var filterMock = new Mock<IFilter<FoodDescription>>();
-
-			IFoodRepository repository = CreateRepository(providerMock.Object, factoryMock.Object, filterMock.Object);
-
-			// Act
-			await repository.Search(title);
-
-			// Assert
-			filterMock.Verify(f => f.Filter(description), Times.Once);
-		}
 		#endregion
 
 		#region Helper methods
-		private static AwsFoodRepository CreateRepository(
-			IAwsProvider provider = null,
-			IFoodDescriptionFactory factory = null,
-			IFilter<FoodDescription> filter = null)
+		private static AwsFoodRepository CreateRepository(IAwsProvider provider = null, IFoodDescriptionFactory factory = null)
 		{
 			return new AwsFoodRepository(
 				provider ?? CreateProvider(),
