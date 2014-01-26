c84fc45f445b5686e20ab31bc8fab6988858570a
Alex Batishchev <abatishchev@gmail.com>
2014-01-26 12:38:00 -0800

    Adding more tests to AwsFoodRepositoryTest

--- OrangeJuice.Server.Core.Test/Data/AwsFoodRepositoryTest.cs
+++ OrangeJuice.Server.Core.Test/Data/AwsFoodRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,10 +19,9 @@ namespace OrangeJuice.Server.Test.Data
 		public async Task SearchTitle_Should_Pass_Title_To_AwsProvider_SearhItems()
 		{
 			// Arrange
-			const string title = "anyTitle";
+			const string title = "title";
 
 			var providerMock = new Mock<IAwsProvider>();
-			providerMock.Setup(c => c.SearchItems(title)).ReturnsAsync(new XElement[0]);
 
 			IFoodRepository repository = CreateRepository(providerMock.Object);
 
@@ -41,13 +41,53 @@ namespace OrangeJuice.Server.Test.Data
 
 			var providerMock = new Mock<IAwsProvider>();
 			providerMock.Setup(c => c.SearchItems(title)).ReturnsAsync(new[] { element });
-
-			var factoryMock = new Mock<IFoodDescriptionFactory>();
+			var factoryMock = CreateFactory(element);
 
 			IFoodRepository repository = CreateRepository(providerMock.Object, factoryMock.Object);
 
 			// Act
-			await repository.SearchTitle(title);
+			(await repository.SearchTitle(title)).ToArray();
+
+			// Assert
+			factoryMock.Verify(f => f.Create(element), Times.Once);
+		}
+		#endregion
+
+		#region SearchBarcode
+		[TestMethod]
+		public async Task SearchBarcode_Should_Pass_Barcode_And_BarcodeType_To_AwsProvider_ItemLookup()
+		{
+			// Arrange
+			const string barcode = "barcode";
+			const BarcodeType barcodeType = BarcodeType.EAN;
+
+			var providerMock = new Mock<IAwsProvider>();
+
+			IFoodRepository repository = CreateRepository(providerMock.Object);
+
+			// Act
+			await repository.SearchBarcode(barcode, barcodeType);
+
+			// Assert
+			providerMock.Verify(c => c.ItemLookup(barcode, barcodeType.ToString()), Times.Once);
+		}
+
+		[TestMethod]
+		public async Task SearchBarcode_Should_Pass_Each_Item_Returned_By_AwsProvider_SearhItems_To_FoodDescriptionFactory_Create()
+		{
+			// Arrange
+			const string barcode = "barcode";
+			const BarcodeType barcodeType = BarcodeType.EAN;
+			XElement element = new XElement("item");
+
+			var providerMock = new Mock<IAwsProvider>();
+			providerMock.Setup(c => c.ItemLookup(barcode, barcodeType.ToString())).ReturnsAsync(new[] { element });
+			var factoryMock = CreateFactory(element);
+
+			IFoodRepository repository = CreateRepository(providerMock.Object, factoryMock.Object);
+
+			// Act
+			(await repository.SearchBarcode(barcode, barcodeType)).ToArray();
 
 			// Assert
 			factoryMock.Verify(f => f.Create(element), Times.Once);
@@ -58,15 +98,15 @@ namespace OrangeJuice.Server.Test.Data
 		private static AwsFoodRepository CreateRepository(IAwsProvider provider = null, IFoodDescriptionFactory factory = null)
 		{
 			return new AwsFoodRepository(
-				provider ?? CreateProvider(),
-				factory ?? new Mock<IFoodDescriptionFactory>().Object);
+				provider ?? new Mock<IAwsProvider>().Object,
+				factory ?? CreateFactory().Object);
 		}
 
-		private static IAwsProvider CreateProvider()
+		private static Mock<IFoodDescriptionFactory> CreateFactory(XElement element = null, FoodDescription description = null)
 		{
-			var providerMock = new Mock<IAwsProvider>();
-			providerMock.Setup(c => c.SearchItems(It.IsAny<string>())).ReturnsAsync(new XElement[0]);
-			return providerMock.Object;
+			var factoryMock = new Mock<IFoodDescriptionFactory>();
+			factoryMock.Setup(f => f.Create(element ?? It.IsAny<XElement>())).Returns(description ?? new FoodDescription());
+			return factoryMock;
 		}
 		#endregion
 	}
