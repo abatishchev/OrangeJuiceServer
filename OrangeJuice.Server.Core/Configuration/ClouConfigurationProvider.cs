62cdb7f27b07fc0061da32dda3a119629998477f
Alex Batishchev <abatishchev@gmail.com>
2014-02-01 21:56:52 -0800

    Deleting AppSettingsConfiugrationProviderTest

--- OrangeJuice.Server.Core/Configuration/ClouConfigurationProvider.cs
+++ OrangeJuice.Server.Core/Configuration/ClouConfigurationProvider.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.WindowsAzure;
-
-namespace OrangeJuice.Server.Configuration
+﻿namespace OrangeJuice.Server.Configuration
 {
 	public sealed class ClouConfigurationProvider : AppSettingsConfigurationProvider
 	{
 		#region IConfigurationProvider members
 		public override string GetValue(string key)
 		{
-			return CloudConfigurationManager.GetSetting(key);
+			return Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting(key);
 		}
 		#endregion
 
