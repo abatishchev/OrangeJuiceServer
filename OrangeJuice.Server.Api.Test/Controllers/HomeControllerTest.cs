858b7998906f29358c21133995a6dfead6f9d7a5
Alex Batishchev <abatishchev@gmail.com>
2014-02-17 23:49:37 -0800

    Removing VersionController in favor of HomeController

--- OrangeJuice.Server.Api.Test/Controllers/HomeControllerTest.cs
+++ OrangeJuice.Server.Api.Test/Controllers/HomeControllerTest.cs
@@ -6,17 +6,19 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using OrangeJuice.Server.Api.Controllers;
+using OrangeJuice.Server.Data;
 
 namespace OrangeJuice.Server.Api.Test.Controllers
 {
 	[TestClass]
 	public class HomeControllerTest
 	{
+		#region Test methods
 		[TestMethod]
 		public void Get_Should_Return_Status_NotFound()
 		{
 			// Assign
-			HomeController controller = new HomeController();
+			HomeController controller = CreateController();
 
 			// Act
 			IHttpActionResult result = controller.Get();
@@ -24,5 +26,29 @@ namespace OrangeJuice.Server.Api.Test.Controllers
 			// Assert
 			result.Should().BeOfType<NotFoundResult>();
 		}
+
+		[TestMethod]
+		public void GetVersion_Should_Return_ApiVersion()
+		{
+			// Arrange
+			ApiVersion expected = new ApiVersion();
+
+			HomeController controller = CreateController(expected);
+
+			// Act
+			IHttpActionResult result = controller.GetVersion();
+			ApiVersion actual = ((OkNegotiatedContentResult<ApiVersion>)result).Content;
+
+			// Assert
+			actual.Should().Be(expected);
+		}
+		#endregion
+
+		#region Helper methods
+		private static HomeController CreateController(ApiVersion apiVersion = null)
+		{
+			return ControllerFactory.Create<HomeController>(apiVersion ?? new ApiVersion());
+		}
+		#endregion
 	}
 }
--- OrangeJuice.Server.Api/Controllers/HomeController.cs
+++ OrangeJuice.Server.Api/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 ﻿using System.Web.Http;
 
+using OrangeJuice.Server.Data;
+
 namespace OrangeJuice.Server.Api.Controllers
 {
 	public sealed class HomeController : ApiController
 	{
+		private readonly ApiVersion _apiVersion;
+
+		public HomeController(ApiVersion apiVersion)
+		{
+			_apiVersion = apiVersion;
+		}
+
 		public IHttpActionResult Get()
 		{
 			return NotFound();
+		}
+
+		[Route("api/home/version")]
+		public IHttpActionResult GetVersion()
+		{
+			return Ok(_apiVersion);
 		}
 	}
 }
