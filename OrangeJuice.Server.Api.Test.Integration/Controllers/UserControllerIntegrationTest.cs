8f068faf75ef49699ac1f88678326570a83f1c7c
Alexander Batishchev <abatishchev@gmail.com>
2014-08-28 16:13:28 -0700

    Implementing UserControllerIntegrationTest

--- OrangeJuice.Server.Api.Test.Integration/Controllers/UserControllerIntegrationTest.cs
+++ OrangeJuice.Server.Api.Test.Integration/Controllers/UserControllerIntegrationTest.cs
@@ -1,27 +1,73 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OrangeJuice.Server.Data;
 
 namespace OrangeJuice.Server.Api.Test.Integration.Controllers
 {
-    /*
-<WebTest Name="UserControllerWebTest" Id="25aa26f6-e12c-48ed-bbda-3a38c9c81dd2" Owner="" Priority="2147483647" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
-  <ContextParameters>
-    <ContextParameter Name="host" Value="http://dev.api.orangejuice.mobi" />
-    <ContextParameter Name="appVer" Value="0.0.0.0" />
-    <ContextParameter Name="randomGuid" Value="" />
-  </ContextParameters>
-  <WebTestPlugins>
-    <WebTestPlugin Classname="Microsoft.SystemCenter.Cloud.GsmPlugins.GuidGeneratorWebTestPlugin, Microsoft.VisualStudio.QualityTools.WebTestFramework, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" DisplayName="Generate Guid" Description="Generates a new guid.">
-      <RuleParameters>
-        <RuleParameter Name="ContextParameterName" Value="randomGuid" />
-        <RuleParameter Name="OutputFormat" Value="" />
-      </RuleParameters>
-    </WebTestPlugin>
-  </WebTestPlugins>
-</WebTest>
-    */
-
     [TestClass]
     public class UserControllerIntegrationTest
     {
+        #region Tests
+        [TestMethod]
+        public async Task Get_Api_User_Should_Return_NotFound_When_User_Does_Not_Exist()
+        {
+            // Arrange
+            Guid userId = Guid.NewGuid();
+
+            var query = HttpUtility.ParseQueryString(String.Empty);
+            query.Add("userId", userId.ToString());
+
+            var client = HttpClientFactory.Create();
+            var url = new UriBuilder(client.BaseAddress);
+            url.Path += "/api/user";
+            url.Query = query.ToString();
+
+            // Act
+            var response = await client.GetAsync(url.Uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public async Task Get_Api_User_Should_Return_Ok_When_User_Exists()
+        {
+            // Arrange
+            Guid? userId = await GetFirstUserId();
+
+            var query = HttpUtility.ParseQueryString(String.Empty);
+            query.Add("userId", userId.ToString());
+
+            var client = HttpClientFactory.Create();
+            var url = new UriBuilder(client.BaseAddress);
+            url.Path += "/api/user";
+            url.Query = query.ToString();
+
+            // Act
+            var response = await client.GetAsync(url.Uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        #endregion
+
+        #region Helper methods
+        private static Task<Guid?> GetFirstUserId()
+        {
+            using (var container = new ModelContainer())
+            {
+                return container.Users.Select(u => (Guid?)u.UserId).FirstAsync();
+            }
+        }
+        #endregion
     }
 }
