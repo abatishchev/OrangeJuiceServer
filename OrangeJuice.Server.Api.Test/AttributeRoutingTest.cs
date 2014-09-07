4776ff9f9f06efa93fef5f42df99dd12bfe17a93
Alexander Batishchev <abatishchev@gmail.com>
2014-09-07 19:46:29 +0200

    Adding test for attribute routing

---
+++ OrangeJuice.Server.Api.Test/AttributeRoutingTest.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrangeJuice.Server.Api.Test
+{
+	[TestClass]
+	public class AttributeRoutingTest
+	{
+		#region Test methods
+		[TestMethod]
+		public void Every_Method_Of_ApiController_Returning_IHttpActionResult_Should_Be_Decorated_With_RouteAttribute()
+		{
+			// Arrange
+			// Act
+			var assembly = Assembly.Load("OrangeJuice.Server.Api");
+			var attributes = from t in assembly.GetTypes()
+					 where typeof(ApiController).IsAssignableFrom(t)
+					 from m in t.GetMethods()
+					 where m.ReturnType == typeof(IHttpActionResult)
+					 select m.GetCustomAttribute<RouteAttribute>();
+
+			// Assert
+			attributes.Should().Contain(a => a != null);
+		}
+		#endregion
+	}
+}
