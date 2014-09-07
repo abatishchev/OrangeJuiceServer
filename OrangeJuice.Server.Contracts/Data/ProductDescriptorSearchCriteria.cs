1cccd24282f30e3e1e8788139c7c7ba23dfcc41e
Alexander Batishchev <abatishchev@gmail.com>
2014-09-07 21:40:40 +0200

    Adding ProductDescriptorSearchCriteria

---
+++ OrangeJuice.Server.Contracts/Data/ProductDescriptorSearchCriteria.cs
@@ -0,0 +1,15 @@
+namespace OrangeJuice.Server.Data
+{
+	public class ProductDescriptorSearchCriteria
+	{
+		public string Operation { get; set; }
+
+		public string SearchIndex { get; set; }
+
+		public string[] ResponseGroups { get; set; }
+
+		public string IdType { get; set; }
+
+		public string ItemId { get; set; }
+	}
+}
