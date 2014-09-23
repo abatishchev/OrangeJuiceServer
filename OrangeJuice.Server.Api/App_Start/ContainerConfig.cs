8c43b9e1a7cec8f0e9d9509d42c148b3129ecbed
Alexander Batishchev <abatishchev@gmail.com>
2014-09-22 18:01:15 -0700

    Renaming AwasProductService to PassThruCloudProductService, implementing Get()

--- OrangeJuice.Server.Api/App_Start/ContainerConfig.cs
+++ OrangeJuice.Server.Api/App_Start/ContainerConfig.cs
@@ -204,7 +204,7 @@ namespace OrangeJuice.Server.Api
 			container.RegisterType<IProductRepository, EntityProductRepository>(
 				new HierarchicalLifetimeManager());
 
-			container.RegisterType<IProductService, AwsProductService>(
+			container.RegisterType<IProductService, PassThruCloudProductService>(
 				new HierarchicalLifetimeManager());
 			#endregion
 
