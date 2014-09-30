4b8b13d61c36325c8675b7df7b000a1a3484fd4a
Alexander Batishchev <abatishchev@gmail.com>
2014-09-29 20:57:44 -0700

    Adding ModelContainerTest

---
+++ OrangeJuice.Server.Data.Test/ModelContainerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using OrangeJuice.Server.Configuration;
+
+namespace OrangeJuice.Server.Data.Test
+{
+	[TestClass]
+	public class ModelContainerTest
+	{
+		[TestMethod]
+		public void Ctor_Should_Call_ConfigurationProvider_Get()
+		{
+			// Arrange
+			var providerMock = new Mock<IConfigurationProvider>();
+			providerMock.Setup(p => p.GetValue("sql:ConnectionString")).Returns("name=Test");
+
+			// Act
+			DbContext context = new ModelContainer(providerMock.Object);
+
+			// Assert
+			providerMock.VerifyAll();
+		}
+	}
+}
