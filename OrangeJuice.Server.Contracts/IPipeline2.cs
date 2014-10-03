301d255ecc1de218dfdb27a8a3a18bfe1c2b7425
Alexander Batishchev <abatishchev@gmail.com>
2014-10-03 16:50:35 -0700

    Adding IPipeline2 and IFilter

---
+++ OrangeJuice.Server.Contracts/IPipeline2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OrangeJuice.Server
+{
+    public interface IPipeline
+    {
+        object Execute(object param);
+
+        IPipeline Register<TParam, TResult>(IPipelineFilter<TParam, TResult> filter);
+    }
+
+    public class Pipeline : IPipeline
+    {
+        private readonly ICollection<IPipelineFilter> _filters = new List<IPipelineFilter>();
+
+        public object Execute(object param)
+        {
+            return _filters.Aggregate(param, (p, f) => f.Execute(p));
+        }
+
+        public IPipeline Register<TParam, TResult>(IPipelineFilter<TParam, TResult> filter)
+        {
+            _filters.Add(filter);
+            return this;
+        }
+    }
+}
---
+++ OrangeJuice.Server.Contracts/IPipelineFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OrangeJuice.Server
+{
+    public interface IPipelineFilter
+    {
+        object Execute(object param);
+    }
+
+    public interface IPipelineFilter<in TParam, out TResult> : IPipelineFilter
+    {
+        TResult Execute(TParam param);
+    }
+
+    public class PipelineFilter<TParam, TResult> : IPipelineFilter<TParam, TResult>
+    {
+        private readonly Func<TParam, TResult> _func;
+
+        public PipelineFilter(Func<TParam, TResult> func)
+        {
+            _func = func;
+        }
+
+        object IPipelineFilter.Execute(object param)
+        {
+            return this.Execute((TParam)param);
+        }
+
+        public TResult Execute(TParam param)
+        {
+            return _func(param);
+        }
+    }
+}
