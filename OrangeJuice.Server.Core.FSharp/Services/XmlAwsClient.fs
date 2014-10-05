808cdac1faf891a2d4af7d0f13b198bd6919e36d
Alexander Batishchev <abatishchev@gmail.com>
2014-10-05 09:17:01 -0700

    Renaming F#\XmlAwsClientPipeline to F#\Services\XmlAwsClient

---
+++ OrangeJuice.Server.Core.FSharp/Services/XmlAwsClient.fs
@@ -0,0 +1,22 @@
+﻿namespace OrangeJuice.Server.Services.FSharp
+
+open System.Threading.Tasks
+open System.Xml.Linq
+
+open Factory
+
+open OrangeJuice.Server.Data
+open OrangeJuice.Server.Services
+open OrangeJuice.Server.Web
+
+type XmlAwsClient(urlBuilder : IUrlBuilder, httpClient : IHttpClient, itemSelector : IItemSelector, factory : IFactory<ProductDescriptor, XElement>) =
+    interface IAwsClient with
+        member this.GetItems(searchCriteria : ProductDescriptorSearchCriteria) = this.GetItems(searchCriteria) |> Async.StartAsTask
+
+    member this.GetItems(searchCriteria : ProductDescriptorSearchCriteria) = async {
+            let url = urlBuilder.BuildUrl(searchCriteria)
+            let! response = httpClient.GetStringAsync(url) |> Async.AwaitTask
+            let items = itemSelector.SelectItems(response)
+            let arr = Seq.map factory.Create items
+            return arr
+        }
