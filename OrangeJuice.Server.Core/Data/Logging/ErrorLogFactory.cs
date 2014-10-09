2b438ed301b7fb34ba64bbc19a8951badffb4bdb
Alexander Batishchev <abatishchev@gmail.com>
2014-10-08 20:03:34 -0700

    Replacing ConfigurationEntityErrorLog with ErrorLogFactory

---
+++ OrangeJuice.Server.Core/Data/Logging/ErrorLogFactory.cs
@@ -0,0 +1,29 @@
+﻿using Elmah;
+
+using OrangeJuice.Server.Configuration;
+
+namespace OrangeJuice.Server.Data.Logging
+{
+	public sealed class ErrorLogFactory : Factory.IFactory<ErrorLog>
+	{
+		private readonly IEnvironmentProvider _environmentProvider;
+		private readonly IConnectionStringProvider _connectionStringProvider;
+
+		public ErrorLogFactory(IEnvironmentProvider environmentProvider, IConnectionStringProvider connectionStringProvider)
+		{
+			_environmentProvider = environmentProvider;
+			_connectionStringProvider = connectionStringProvider;
+		}
+
+		public ErrorLog Create()
+		{
+			switch (_environmentProvider.GetCurrentEnvironment())
+			{
+				case Environment.Production:
+					return new Elmah.Contrib.EntityFramework.EntityErrorLog(_connectionStringProvider.GetDefaultConnectionString());
+				default:
+					return new TraceErrorLog();
+			}
+		}
+	}
+}
---
+++ OrangeJuice.Server.Core/Data/Logging/TraceErrorLog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+using Elmah;
+
+namespace OrangeJuice.Server.Data.Logging
+{
+	public class TraceErrorLog : ErrorLog
+	{
+		public override string Log(Error error)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override ErrorLogEntry GetError(string id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override int GetErrors(int pageIndex, int pageSize, IList errorEntryList)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
