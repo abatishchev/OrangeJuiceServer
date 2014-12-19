351ace5a9acc234f95c325be0471312cac21a928
Alexander Batishchev <abatishchev@gmail.com>
2014-12-18 22:54:55 -0800

    Updating EncodedQueryBuilder with Seq.Iter

--- OrangeJuice.Server.Core.FSharp/Web/EncodedQueryBuilder.fs
+++ OrangeJuice.Server.Core.FSharp/Web/EncodedQueryBuilder.fs
@@ -9,8 +9,7 @@ type EncodedQueryBuilder(urlEncoder : IUrlEncoder) =
     interface IQueryBuilder with
         member this.BuildQuery(args : IEnumerable<KeyValuePair<string, string>>) : string =
             let coll = HttpUtility.ParseQueryString(String.Empty, urlEncoder)
-            for arg in args do
-                 coll.Add(arg.Key, arg.Value)
+            args |> Seq.iter (fun arg -> coll.Add(arg.Key, arg.Value))
             coll.ToString()
 
         member this.SignQuery(query : string, signature : string) =
