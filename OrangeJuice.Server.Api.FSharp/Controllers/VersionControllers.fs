57118b981ebb73b6f0b91a383358d4e4c5732723
Alexander Batishchev <abatishchev@gmail.com>
2014-12-24 22:44:00 -0800

    Adding VersionController.fs

---
+++ OrangeJuice.Server.Api.FSharp/Controllers/VersionControllers.fs
@@ -0,0 +1,12 @@
+﻿namespace OrangeJuice.Server.Api.FSharp.Controllers
+
+open System.Web.Http
+
+open OrangeJuice.Server.Data.Models
+
+type VersionController(apiVersion : ApiVersion) =
+    inherit ApiController()
+
+    [<Route("api/version")>]
+    member this.GetVersion() : IHttpActionResult  =
+        this.Ok(apiVersion) :> IHttpActionResult
