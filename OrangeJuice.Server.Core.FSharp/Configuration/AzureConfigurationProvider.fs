a2d22765e14d1d001dfac6fa3a61d9ff78723823
Alexander Batishchev <abatishchev@gmail.com>
2015-07-12 10:53:36 -0700

    Updatig nuget packages, fixing the build

--- OrangeJuice.Server.Core.FSharp/Configuration/AzureConfigurationProvider.fs
+++ OrangeJuice.Server.Core.FSharp/Configuration/AzureConfigurationProvider.fs
@@ -5,4 +5,4 @@ open OrangeJuice.Server.Configuration
 type AzureConfigurationProvider() =
     interface IConfigurationProvider with
         member this.GetValue(key : string) : string =
-            Microsoft.WindowsAzure.CloudConfigurationManager.GetSetting(key)
+            Microsoft.Azure.CloudConfigurationManager.GetSetting(key)
