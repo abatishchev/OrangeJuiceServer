1395414ddccbf2c6639c4672c05bc57b1fd67282
Alexander Batishchev <abatishchev@gmail.com>
2015-07-12 08:26:43 -0700

    Improving TraceRequestHandler

--- OrangeJuice.Server.Api/Handlers/TraceRequestHandler.cs
+++ OrangeJuice.Server.Api/Handlers/TraceRequestHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,14 +11,17 @@ namespace OrangeJuice.Server.Api.Handlers
 		private const string MsHttpContextKey = "MS_HttpContext";
 
 		private readonly ITraceRequestRepository _repository;
+		private readonly IDateTimeProvider _dateTimeProvider;
 
-		public TraceRequestHandler(ITraceRequestRepository repository)
+		public TraceRequestHandler(ITraceRequestRepository repository, IDateTimeProvider dateTimeProvider)
 		{
 			_repository = repository;
+			_dateTimeProvider = dateTimeProvider;
 		}
 
 		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
 		{
+			// TODO: await here?
 			Task.Factory.StartNew(async () => await TraceRequest(request), cancellationToken);
 
 			return base.SendAsync(request, cancellationToken);
@@ -28,7 +30,7 @@ namespace OrangeJuice.Server.Api.Handlers
 		private Task TraceRequest(HttpRequestMessage request)
 		{
 			return _repository.Add(
-				DateTime.UtcNow,
+				_dateTimeProvider.GetNow(),
 				request.RequestUri.ToString(),
 				request.Method.Method,
 				((System.Web.HttpContextWrapper)request.Properties[MsHttpContextKey]).Request.UserHostAddress,
