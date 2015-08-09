695512bd7a5640db35fb1f6d147941db779ea84b
Alexander Batishchev <abatishchev@gmail.com>
2015-08-08 21:23:14 -0700

    Extracting AzureContainerClient from AzureClient

--- OrangeJuice.Server.Contracts/Services/IAzureClient.cs
+++ OrangeJuice.Server.Contracts/Services/IAzureClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OrangeJuice.Server.Services
@@ -6,6 +7,8 @@ namespace OrangeJuice.Server.Services
 	public interface IAzureClient
 	{
 		Task<string> GetBlobFromContainer(string containerName, string fileName);
+
+		Task<IEnumerable<string>> GetBlobsFromContainer(string containerName);
 
 		Task PutBlobToContainer(string containerName, string fileName, string content);
 
--- OrangeJuice.Server.Core/Services/AzureClient.cs
+++ OrangeJuice.Server.Core/Services/AzureClient.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.WindowsAzure.Storage;
@@ -10,26 +12,20 @@ namespace OrangeJuice.Server.Services
 {
 	public sealed class AzureClient : IAzureClient
 	{
-		#region Fields
 		private static readonly TimeSpan Year = TimeSpan.FromDays(365);
 
-		private readonly AzureOptions _azureOptions;
 		private readonly IBlobClient _blobClient;
-		#endregion
+		private readonly IAzureContainerClient _containerClient;
 
-		#region Ctor
-		public AzureClient(AzureOptions azureOptions, IBlobClient blobClient)
+		public AzureClient(IBlobClient blobClient, IAzureContainerClient containerClient)
 		{
-			_azureOptions = azureOptions;
 			_blobClient = blobClient;
+			_containerClient = containerClient;
 		}
-		#endregion
-
-		#region IAzureClient members
 
 		public async Task<string> GetBlobFromContainer(string containerName, string fileName)
 		{
-			ICloudBlob blob = await GetBlockReference(containerName, fileName);
+			ICloudBlob blob = await _containerClient.GetBlockReference(containerName, fileName);
 
 			bool exists = await blob.ExistsAsync();
 			if (!exists)
@@ -38,58 +34,31 @@ namespace OrangeJuice.Server.Services
 			return await _blobClient.Read(blob);
 		}
 
+		public async Task<IEnumerable<string>> GetBlobsFromContainer(string containerName)
+		{
+			var container = await _containerClient.GetContainer(containerName);
+			var tasks = container.ListBlobs()
+								 .Cast<CloudBlockBlob>()
+								 .Select(b => b.DownloadTextAsync());
+			return await Task.WhenAll(tasks);
+		}
+
 		public async Task PutBlobToContainer(string containerName, string fileName, string content)
 		{
-			ICloudBlob blob = await GetBlockReference(containerName, fileName);
+			ICloudBlob blob = await _containerClient.GetBlockReference(containerName, fileName);
 			blob.Properties.CacheControl = CreateCacheControl(Year);
 			await _blobClient.Write(blob, content);
 		}
 
 		public async Task<Uri> GetBlobUrl(string containerName, string fileName)
 		{
-			ICloudBlob blob = await GetBlobReference(containerName, fileName);
+			ICloudBlob blob = await _containerClient.GetBlobReference(containerName, fileName);
 			return blob.Uri;
-		}
-		#endregion
-
-		#region Methods
-		// TODO: refactor out?
-		private async Task<CloudBlobContainer> GetContainer(string containerName)
-		{
-			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_azureOptions.ConnectionString);
-			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-			CloudBlobContainer container = blobClient.GetContainerReference(containerName);
-			bool exists = await container.ExistsAsync();
-			if (!exists)
-				throw new InvalidOperationException(String.Format("Container {0} doesn't exist", containerName));
-
-			return container;
-		}
-
-		private async Task<ICloudBlob> GetBlobReference(string containerName, string blobName)
-		{
-			CloudBlobContainer container = await GetContainer(containerName);
-			string fileName = CreateFileName(blobName);
-			return container.GetBlobReferenceFromServer(fileName);
-		}
-
-		private async Task<CloudBlockBlob> GetBlockReference(string containerName, string blobName)
-		{
-			CloudBlobContainer container = await GetContainer(containerName);
-			string fileName = CreateFileName(blobName);
-			return container.GetBlockBlobReference(fileName);
 		}
 
 		private static string CreateCacheControl(TimeSpan timeSpan)
 		{
 			return String.Format("public, max-age={0}", timeSpan.TotalMilliseconds);
 		}
-
-		private static string CreateFileName(string blobName)
-		{
-			return String.Format("{0}.json", blobName);
-		}
-		#endregion
 	}
 }
