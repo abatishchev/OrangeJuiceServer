8d02f96ac4aa5fe94fd07f8b039f94083e462f6c
Alexander Batishchev <abatishchev@gmail.com>
2015-08-09 20:19:46 -0700

    Referencing IFactory.SimpleInjector

--- OrangeJuice.Server.Api/App_Start/ContainerConfig.cs
+++ OrangeJuice.Server.Api/App_Start/ContainerConfig.cs
@@ -13,7 +13,9 @@ using System.Xml.Linq;
 
 using Drum;
 using Elmah;
+
 using Factory;
+using Factory.SimpleInjector;
 
 using FluentValidation;
 using FluentValidation.Attributes;
@@ -244,26 +246,6 @@ namespace OrangeJuice.Server.Api
 
 	internal static class ContainerExtensions
 	{
-		public static void RegisterFactory<T, TFactory>(this Container container)
-			where T : class
-			where TFactory : class, IFactory<T>
-		{
-			container.Register<IFactory<T>, TFactory>(Lifestyle.Singleton);
-			container.Register(() => container.GetInstance<TFactory>().Create());
-		}
-
-		public static void RegisterFactory<T, U, TFactory>(this Container container)
-		   where TFactory : class, IFactory<T, U>
-		{
-			container.Register<IFactory<T, U>, TFactory>(Lifestyle.Singleton);
-		}
-
-		public static void RegisterFactory<T, U1, U2, TFactory>(this Container container)
-		   where TFactory : class, IFactory<T, U1, U2>
-		{
-			container.Register<IFactory<T, U1, U2>, TFactory>(Lifestyle.Singleton);
-		}
-
 		public static void AddService<T>(this Container container, ServicesContainer services)
 			where T : class
 		{
