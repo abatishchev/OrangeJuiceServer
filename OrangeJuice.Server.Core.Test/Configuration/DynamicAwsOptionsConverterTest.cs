49d5f7cbcc88292d721c0c7c9186bb9995bad036
Alexander Batishchev <abatishchev@gmail.com>
2015-08-13 21:56:25 -0700

    Adding DynamicAwsOptionsConverterTest

---
+++ OrangeJuice.Server.Core.Test/Configuration/DynamicAwsOptionsConverterTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.WindowsAzure.Storage.Table;
+using OrangeJuice.Server.Configuration;
+using Xunit;
+
+namespace OrangeJuice.Server.Test.Configuration
+{
+	public class DynamicAwsOptionsConverterTest
+	{
+		[Theory]
+		[InlineData(typeof(DynamicAwsOptionsConverter))]
+		[InlineData(typeof(FSharp.Configuration.DynamicAwsOptionsConverter))]
+		public void Convert_And_ConverterBack_Should_Convert_DynamicTableEntity_To_AwsOptions_And_Back(Type type)
+		{
+			// Arrange
+			var expected = new AwsOptions
+			{
+				AssociateTag = "associateTag",
+				AccessKey = "accessKey",
+				SecretKey = "secretKey",
+				RequestLimit = TimeSpan.MinValue
+			};
+
+			var converter = CreateConverter(type);
+
+			// Act
+			DynamicTableEntity entity = converter.ConvertBack(expected);
+			AwsOptions actual = converter.Convert(entity);
+
+			// Assert
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		private static IConverter<DynamicTableEntity, AwsOptions> CreateConverter(Type type)
+		{
+			return (IConverter<DynamicTableEntity, AwsOptions>)Activator.CreateInstance(type);
+		}
+	}
+}
--- OrangeJuice.Server.Core.Test/Data/JsonProductDescriptorConverterTest.cs
+++ OrangeJuice.Server.Core.Test/Data/JsonProductDescriptorConverterTest.cs
@@ -11,14 +11,13 @@ namespace OrangeJuice.Server.Test.Data
 {
 	public class JsonProductDescriptorConverterTest
 	{
-		#region Tests
 		[Theory]
 		[InlineData(typeof(JsonProductDescriptorConverter))]
 		[InlineData(typeof(FSharp.Data.JsonProductDescriptorConverter))]
-		public void Convert_And_ConverterBack_Should_Convert_To_ProductDescriptor_To_String_And_Back_To_ProductDescriptor(Type type)
+		public void Convert_And_ConverterBack_Should_Convert_ProductDescriptor_To_String_And_Back(Type type)
 		{
 			// Arrange
-			ProductDescriptor expected = new ProductDescriptor
+			var expected = new ProductDescriptor
 			{
 				ProductId = Guid.Parse("8f76a6f5-037f-41b4-9837-de9b5c5ff926"),
 				SourceProductId = "A00BCD123EF",
@@ -38,13 +37,10 @@ namespace OrangeJuice.Server.Test.Data
 			// Assert
 			actual.ShouldBeEquivalentTo(expected);
 		}
-		#endregion
 
-		#region Helper methods
 		private static IConverter<string, ProductDescriptor> CreateConverter(Type type)
 		{
 			return (IConverter<string, ProductDescriptor>)Activator.CreateInstance(type);
 		}
-		#endregion
 	}
 }
